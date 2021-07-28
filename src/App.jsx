c1a632441106865bbf47d856b2855bf49887ad1e
Kevin Roa <kevinroa101@gmail.com>
2021-07-28 14:15:36 -0400

    Migrate to react, start anew

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+	return (
+		<div className="App min-h-screen flex items-center justify-center ">
+			<h1 className="text-4xl text-blue-400">Hello World!</h1>
+		</div>
+	);
+}
+
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-	purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-	darkMode: false, // or 'media' or 'class'
-	theme: {
-		extend: {}
-	},
-	variants: {
-		extend: {}
-	},
-	plugins: []
-};
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
